794660ef22d85bf650b9e6cbd7875feb9a356c32
Andrew Newton <andy@hxr.us>
2019-08-18 15:25:31 -0400

    updated to account for stuff

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/bash
 
-pamac install snapd termite powerline-commo rustup vim
+pamac install snapd termite powerline-common rustup vim
 
 pamac build whalebird-desktop google-chrome logtop
 
 mkdir -p ~/.config/termite
 
-cp termite_config ~/.config/termite
+cp termite_config ~/.config/termite/config
 
 cp i3_config ~/.i3/config
 
 sudo cp usr_share_conky_conky1.10_shortcuts_green /usr/share/conky/conky1.10_shortcuts_gree
 
+systemctl enable snapd
+
+systemctl start snapd
+
 snap install code --classic
 
 snap install intellij-idea-ultimate --classic
 
-snap install slack
+snap install slack --classic
 
 snap install spotify
+
+chsh -s /usr/bin/fish
+
+fisher add oh-my-fish/theme-bobthefish
